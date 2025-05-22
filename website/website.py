d8f7a8a4c8f658d32fc934cb845b8f95b2167ecb
DiniskaWolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2025-05-22 11:49:50 +0200

    Add initial website structure with configuration and main application logic

---
+++ website/website.py
@@ -0,0 +1,45 @@
+import reflex as rx
+
+from rxconfig import config
+
+
+class State(rx.State):
+    ...
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            rx.link(
+                rx.button("Pfotenclub"),
+                href="https://pfotenclub.eu",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
+
+
+app = rx.App(
+    theme=rx.theme(
+        brand_color="blue",
+        font="Inter",
+        logo_url="https://reflex.dev/logo.png",
+    ),
+)
+app.add_page(index)
